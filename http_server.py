2fc0dc1791d949728f052d7537e5a02ed9f130fc
Randsw <randsw@mail.ru>
2020-12-01 14:56:53 +0300

    Working http server and db connection. Prepare docker-compose file

--- http_server.py
+++ http_server.py
@@ -1,8 +1,23 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+from postgres_adapter import Database
+import json
 
+db = Database()
+
+def Post_to_db(data):
+    if db.connect() == 0:
+        db.insert_player(tuple(data.values()))
+        db.close_db_connection()       
+
+def Get_from_db(id):
+    if db.connect() == 0:
+        player_data = db.read_player(id)
+        db.close_db_connection() 
+        return player_data
 
 class Server(BaseHTTPRequestHandler):
+
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -11,14 +26,16 @@ class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        player_record = Get_from_db(int(self.path.replace("/","")))
+        self.wfile.write(str(player_record).encode('utf-8'))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                      str(self.path), str(self.headers), post_data.decode('utf-8'))
-
+        player_record = json.loads(post_data.decode('utf-8'))
+        Post_to_db(player_record)
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
@@ -26,6 +43,12 @@ class Server(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=Server, port=8080):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
+    #db = Database()
+    if db.connect() == 0:
+        db.create_table_players("players")
+        db.close_db_connection()
+    else:
+        logging.error('Cannot connect to db')
     httpd = server_class(server_address, handler_class)
     logging.info('Starting httpd...\n')
     try:
--- postgres_adapter.py
+++ postgres_adapter.py
@@ -1,9 +1,61 @@
 import psycopg2
 
-con = psycopg2.connect(
-  database="postgres",
-  user="postgres",
-  password="",
-  host="127.0.0.1",
-  port="5432"
-)
+
+class Database():
+
+    def __init__(self, database="postgres", user="postgres", password="postgres", host="127.0.0.1", port="5432"):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.con = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.con = psycopg2.connect(
+                database="postgres",
+                user="postgres",
+                password="postgres",
+                host="127.0.0.1",
+                port="5432"  
+        )
+            self.cursor = self.con.cursor()
+            return 0
+        except (Exception, psycopg2.Error):
+            return -1
+
+    def create_table_players(self, table_name):
+        self.cursor.execute("select exists(select * from information_schema.tables where table_name ='{}')".format(table_name.lower()))
+        z = self.cursor.fetchone()[0]
+        if not bool(z):
+            create_table_query = '''CREATE TABLE {}
+            (ID INT PRIMARY KEY     NOT NULL,
+            NAME           TEXT    NOT NULL,
+            ROLE           TEXT    NOT NULL,
+            COUNTRY        TEXT    NOT NULL, 
+            NUMBER         INT NOT NULL); '''.format(table_name.lower())
+            self.cursor.execute(create_table_query)
+            self.con.commit()
+            return("Table created")
+        else:
+            return("Table already exist")
+
+
+    def insert_player(self, data):
+        postgres_insert_query = """ INSERT INTO players (ID, NAME, ROLE, COUNTRY, NUMBER) VALUES (%s,%s,%s,%s,%s)"""
+        record_to_insert = data
+        self.cursor.execute(postgres_insert_query, record_to_insert)
+        self.con.commit()
+
+    def read_player(self, id):
+        postgreSQL_select_Query = "select * from players where id = %s"
+        self.cursor.execute(postgreSQL_select_Query, (id,))
+        player_records = self.cursor.fetchall()
+        return player_records
+
+    def close_db_connection(self):
+        if self.con:
+            self.cursor.close()
+            self.con.close()
